91426936ee1d53aa4e9aa772a1de640587b73232
Zeeshan Ali <zeeshan2193@gmail.com>
2021-03-24 18:14:46 +0500

    New Jamstack Todo List
    New Jamstack Todo List

---
+++ packages/www/.cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\zeshi\\Documents\\GitHub\\NewJamstackTodoApp\\packages\\www\\.cache\\dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\zeshi\\Documents\\GitHub\\NewJamstackTodoApp\\packages\\www\\src\\pages\\index.js"))
+}
+
